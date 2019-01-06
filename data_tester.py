5856f87e1b4b276ca84c01869fa4de9fe182f111
Anudeep Golla <anudeepgolla3@gmail.com>
2019-01-05 21:27:31 -0700

    analytics engine ready for regression testing

--- data_tester.py
+++ data_tester.py
@@ -3,16 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-df = pd.read_csv('data/genomic_data_set_v1.csv')
+df = pd.read_csv('data/processed_genomic_data_set_v2.csv')
 
-# cols = list(df.columns)
-#
-# print(df.head(5))
-# print(df.dtypes)
-# print(cols)
-# print(len(cols))
-# print(df.nunique())
+cols = list(df.columns)
 
-# df = df['pause_G'] - df['pause_C']
-#
-# print(df.head(30))
+print(df.head(5))
+print(df.dtypes)
+print(cols)
+print(len(cols))
+print(df.nunique())
+
+
